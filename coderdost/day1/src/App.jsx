7e319c036d69c371e822c5f8eddc62343381fef3
amandeepdhillon123 <amandeep24397@gmail.com>
2023-04-23 09:37:58 +0530

    react basics

---
+++ coderdost/day1/src/App.jsx
@@ -0,0 +1,13 @@
+
+  import {Video, Thumbnail} from "./component/Thumbnail";
+function App(){
+
+  return(
+    <div>
+      <h1> Hello</h1>
+      <Thumbnail />
+      <Video />
+    </div>
+  )
+}
+export default App;
